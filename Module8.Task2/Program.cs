8e21664de65b16e8337b83a146897b5a3098a2eb
DenisValuev <valuev.denis91@mail.ru>
2024-09-03 12:08:05 +0300

    Module8-master: add Task2

--- Module8.Task2/Program.cs
+++ Module8.Task2/Program.cs
@@ -2,9 +2,39 @@
 {
     internal class Program
     {
+        const string path = "D:\\Личные документы";
+        public static void ShowTotalSizeFolder(string _path)
+        {
+            long totalSize = 0;
+            if (Directory.Exists(_path))
+            {
+                DirectoryInfo directoryInfo = new DirectoryInfo(path);
+                string[] files = Directory.GetFiles(path, ".", SearchOption.AllDirectories);
+                if (files.Length > 0)
+                {
+                    foreach (string file in files)
+                    {
+                        FileInfo fileInfo = new FileInfo(file);
+                        totalSize += fileInfo.Length;
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Неверно указан путь к директории");
+            }
+            Console.WriteLine($"Размер папки равен: {totalSize} байт");
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            try
+            {
+                ShowTotalSizeFolder(path);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
